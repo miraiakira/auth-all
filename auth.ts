21a85b3cd7cfde567c6079f5faefe24126607369
= <=>
2024-01-07 23:47:13 +0900

    settings

--- auth.ts
+++ auth.ts
@@ -6,12 +6,14 @@ import { db } from '@/lib/db'
 import authConfig from '@/auth.config'
 import { UserRole } from '@prisma/client'
 import {getTwoFactorConfirmationByUserId} from '@/data/two-factor-confirmation'
+import { getAccountByUserId } from './data/account'
 
 export const {
   handlers: { GET, POST },
   signIn,
   signOut,
-  auth
+  auth,
+  update,
 } = NextAuth({
   pages: {
     signIn: '/auth/login',
@@ -51,26 +53,41 @@ export const {
       return true
     },
     async session({session, token}){
-      console.log({
-        sessionToken: token
-      })
       if(token.sub && session.user) {
         session.user.id = token.sub;
       }
+      
       if(token.role && session.user) {
         session.user.role = token.role as UserRole;
       }
+
       if(session.user) {
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean
       }
+
+      if(session.user) {
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.isOAuth = token.isOAuth as boolean
+      }
+
       return session
     },
     async jwt({token}) {
       if(!token.sub) return token
+
       const existingUser = await getUserById(token.sub)
+
       if(!existingUser) return token
+
+      const existingAccount = await getAccountByUserId(existingUser.id)
+
+      token.isOAuth = !!existingAccount
+      token.name = existingUser.name
+      token.email = existingUser.email
       token.role = existingUser.role
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled
+      
       return token;
     }
   },
---
+++ components/auth/role-gate.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React from 'react';
+import { UserRole } from '@prisma/client';
+
+import { useCurrentRole } from '@/hooks/use-current-role';
+import { FormError } from '@/components/form-error';
+
+interface RoleGateProps {
+  children: React.ReactNode;
+  allowedRole: UserRole;
+}
+
+export function RoleGate({ children, allowedRole }: RoleGateProps) {
+  const role = useCurrentRole();
+
+  if (role !== allowedRole) {
+    return (
+      <FormError message="You do not have permission to view this content!" />
+    );
+  }
+  return <>{children}</>;
+}
